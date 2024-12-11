ee98b3cfa72c03094a1e571ce12635ebacf45faa
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-12-11 11:00:20 +0100

    Post +

--- EJEMPLOS/api_rest_01/src/main/java/com/salesianostriana/dam/api_rest_01/MainController.java
+++ EJEMPLOS/api_rest_01/src/main/java/com/salesianostriana/dam/api_rest_01/MainController.java
@@ -1,10 +1,15 @@
 package com.salesianostriana.dam.api_rest_01;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+
+    // Imita una clase.
+    record Producto(Long id, String nombre) {}
 
     @GetMapping("/hello")
     public String sayHello() {
@@ -16,7 +21,10 @@ public class MainController {
         return new Producto(123L,"Un producto muy molón");
     }
 
-    record Producto(Long id, String nombre) {
-
+    @PostMapping("/producto")
+    public Producto addProducto(@RequestBody Producto producto) {
+        return producto;
     }
+
+
 }
