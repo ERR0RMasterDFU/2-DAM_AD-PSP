3a51ee937ad2c72e55b3de23410200b5448c6c68
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2025-02-11 13:27:04 +0100

    Cheto de hierro

---
+++ EJERCICIOS/SEGURIDAD/ejercicio_seguridad_2/src/main/java/com/salesianostriana/dam/ejercicio_seguridad_2/user/dto/CreateUserRequest.java
@@ -0,0 +1,10 @@
+package com.salesianostriana.dam.ejercicio_seguridad_2.user.dto;
+
+public class CreateUserRequest {
+    private String username;
+    private String password;
+    private String verifyPassword;
+    private String avatar;
+    private String fullName;
+
+}
--- EJERCICIOS/SEGURIDAD/ejercicio_seguridad_2/src/main/java/com/salesianostriana/dam/ejercicio_seguridad_2/user/model/User.java
+++ EJERCICIOS/SEGURIDAD/ejercicio_seguridad_2/src/main/java/com/salesianostriana/dam/ejercicio_seguridad_2/user/model/User.java
@@ -1,24 +1,20 @@
 package com.salesianostriana.dam.ejercicio_seguridad_2.user.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.NaturalId;
 import org.hibernate.annotations.Parameter;
-import org.hibernate.annotations.Type;
+import org.hibernate.proxy.HibernateProxy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-//import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Collection;
-import java.util.EnumSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -26,7 +22,10 @@ import java.util.stream.Collectors;
 @Entity
 @Table(name="user_entity")
 @EntityListeners(AuditingEntityListener.class)
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -116,5 +115,24 @@ public class User implements UserDetails {
     @Override
     public boolean isEnabled() {
         return enabled;
+    }
+
+
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id)
+                && getUsername() != null && Objects.equals(getUsername(), user.getUsername());
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(username);
     }
 }
